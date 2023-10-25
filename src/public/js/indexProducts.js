9f9b475f963bd3700896f8d252c67b81bd924bd9
Rebecca Orona <becca@developsys.ar>
2023-10-25 18:47:03 -0300

    Agregado de modificacion de productos para premium y admins

--- src/public/js/indexProducts.js
+++ src/public/js/indexProducts.js
@@ -5,10 +5,89 @@ const socket = socketIoClient();
 var cartId = null;
 let purchaser = null;
 let baseUrl = 'https://back-end-nodejs-coderhouse-development.up.railway.app';
+const updateProductForm = document.querySelector('.update-product-form');
 
-function showUpdateProduct(){
-
+function addAfter(referenceNode, newNode) {
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
+
+function showUpdateProduct() {
+  // Find the "Agregar al Carrito" button
+  const addToCartButtons = document.querySelectorAll('.addToCartButton');
+
+  // Iterate through the buttons and add an "Update Product" button after each one
+  addToCartButtons.forEach(addToCartButton => {
+      const updateProductButton = document.createElement('div');
+      updateProductButton.classList.add('updateProductButton'); // You can define the CSS styles for this class
+      updateProductButton.innerHTML = '<i class="fas fa-pencil-alt"></i> Actualizar Producto';
+      
+
+      // Add a click event listener to the "Update Product" button
+      updateProductButton.addEventListener('click', () => {
+        updateProductForm.style.display = 'block';
+      });
+
+      // Insert the "Update Product" button after the "Agregar al Carrito" button
+      addAfter(addToCartButton, updateProductButton);
+  });
+}
+
+updateProductForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  // Get the product details from the form input fields
+  const title = document.querySelector('#title').value;
+  const description = document.querySelector('#description').value;
+  const code = document.querySelector('#code').value;
+  const price = document.querySelector('#price').value;
+  const status = document.querySelector('#status').value;
+  const stock = document.querySelector('#stock').value;
+  const category = document.querySelector('#category').value;
+  const thumbnail = document.querySelector('#thumbnail').value;
+
+  // Get the productId from the "Update Product" button
+  const productId = event.target.getAttribute('data-productid');
+
+  // Prepare the data for the PUT request
+  const productData = {
+      title,
+      description,
+      code,
+      price,
+      status,
+      stock,
+      category,
+      thumbnail
+  };
+
+  try {
+    const response = await fetch(`/api/products/${productId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify(productData)
+    });
+
+    if (response.ok) {
+      updateProductForm.style.display = 'none';
+      Swal.fire({
+        icon: 'success',
+        title: 'Producto modificado correctamente',
+        text: `El producto con el ID ${productId} fue modificado con exito!`,
+        toast: true,
+        position: 'top-right',
+        showConfirmButton: false,
+        timer: 3000
+      });
+    } else {
+        console.error('Error updating product:', response.statusText);
+    }
+  } catch (error) {
+      console.error('Error updating product:', error);
+  }
+});
 
 function getCookie(name) {
   const value = `; ${document.cookie}`;
@@ -28,7 +107,9 @@ socket.on('newClientConnected', async() => {
       }) 
       .then(response => response.json())
       .then(userData => {
-          
+        if (userData.payload.role === 'admin' || userData.payload.role === 'premium') {
+          showUpdateProduct();
+        }
         purchaser = userData.payload.email;
       
         fetch(`/api/carts/findCartByPurchaser/${purchaser}`)
